79acab5784a330192d5855779eea7cd2c0b4a00b
Saad ali <142331196+syedsaadaly@users.noreply.github.com>
2024-03-01 14:15:37 +0500

    Add files via upload

---
+++ JS 2/app.js
@@ -0,0 +1,56 @@
+// 1
+// var username;
+
+// 2
+// var myName = "Syed Saad Ali" ;
+
+// 3
+// var message = "Hello World";
+// alert(message)
+
+// 4
+//   var studentName = prompt("Enter your name:");
+//   var studentAge = +prompt("Enter your age:");
+//   alert("Your Name: " + studentName )
+//   alert("Your Age: " + studentAge);
+//   alert("Certified Mobile Application Development")
+
+// 5
+// var p1="PIZZA";
+// var p2="PIZZ";
+// var p3="PIZ";
+// var p4="PI";
+// var p5="P";
+// alert(p1 + \n+ p2 + \n + p3 + \n +p4 + \n" +p5 );
+
+// 6
+// var email = "syedsaadaly01@gmail.com";
+// alert("My email address is: " + email);
+
+// 7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book: " + book);
+
+// 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// 9
+// var design= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design)
+
+// CHAPTER 3
+//  1
+//   var age = 23; 
+//   alert("I am " + age + " years old");
+
+// 2
+
+// 3
+// var birthYear = 2001;
+// alert('My birth year is '+ birthYear + "\n" + 'Data type of my declared variable is number');
+
+// 4
+// var visitorName = "John Doe";
+//   var productTitle = "T-shirt";
+//   var quantity = 5;
+//   document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
